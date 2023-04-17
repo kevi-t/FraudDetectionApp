3534f7876c2739fe88e1465d076474039d92522c
kevi-t <kevinwafula716@gmail.com>
2023-04-17 10:27:51 +0300

    update check balance

--- src/main/java/fraud/detection/app/services/CheckBalanceService.java
+++ src/main/java/fraud/detection/app/services/CheckBalanceService.java
@@ -7,17 +7,13 @@ import fraud.detection.app.dto.CheckBalanceDTO;
 import fraud.detection.app.dto.SendMoneyMessage;
 import fraud.detection.app.models.Account;
 import fraud.detection.app.models.Transaction;
-import fraud.detection.app.models.User;
 import fraud.detection.app.repositories.AccountRepository;
 import fraud.detection.app.repositories.TransactionRepository;
-import fraud.detection.app.repositories.UserRepository;
 import fraud.detection.app.responses.UniversalResponse;
+import fraud.detection.app.utils.HelperUtility;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @Slf4j
@@ -26,43 +22,27 @@ public class CheckBalanceService {
     private final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
     private  final TwilioConfiguration twilioConfiguration;
+    public final HelperUtility helperUtility;
     public UniversalResponse response;
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    String referenceCode = HelperUtility.referenceCodeGenerator();
 
     @Autowired
-    public CheckBalanceService(AccountRepository accountRepository, TransactionRepository transactionRepository, TwilioConfiguration twilioConfiguration, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public CheckBalanceService(AccountRepository accountRepository,
+                               TransactionRepository transactionRepository,
+                               TwilioConfiguration twilioConfiguration, HelperUtility helperUtility) {
         this.accountRepository = accountRepository;
         this.transactionRepository = transactionRepository;
         this.twilioConfiguration = twilioConfiguration;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.helperUtility = helperUtility;
     }
 
     public UniversalResponse checkBalance(CheckBalanceDTO request) {
         try {
-            Account accountNumber = accountRepository.findByAccountNumber(request.getAccountNumber());
-            String EnteredPin = request.getPin();
-            User user = userRepository.findUserBymobileNumber(request.getAccountNumber());
-            String DatabasePin = user.getPin();
-            if (accountNumber == null) {
-                return UniversalResponse.builder().message("Account not found,please try again").build();
-            }
-            else if (passwordEncoder.matches(EnteredPin, DatabasePin)){
-                System.out.println("You Entered The wrong Pin");
-                return UniversalResponse.builder().message("You Entered The wrong Pin!").status(0).build();
-            }
-            else {
+              Account accountNumber = accountRepository.findByAccountNumber(request.getAccountNumber());
+            if (helperUtility.checkPin(request.getPin(),request.getAccountNumber())){
                 try{
-                    //generating unique reference code
-                    UUID uuid = UUID.randomUUID();
-                    String randomUUIDString = uuid.toString();
-                    String referenceCode = "TUCN" + randomUUIDString;
 
                     Transaction transaction = Transaction.builder()
-                            .receiverAccount(request.getAccountNumber())
-                            .senderAccount(request.getAccountNumber())
-                            .transactionAmount(accountNumber.getAccountBalance())
                             .ReferenceCode(referenceCode)
                             .transactionType("CHECK BALANCE")
                             .Status("0")
@@ -77,8 +57,9 @@ public class CheckBalanceService {
                             .build();
                     System.out.println(smsRequest);
                     try {
-                        Message twilioMessage = Message.creator(new PhoneNumber(smsRequest.getReceiverPhoneNumber()),
-                                new PhoneNumber(twilioConfiguration.getTrial_number()), "Account balance request. Ksh" + accountNumber.getAccountBalance()).create();
+                        Message.creator(new PhoneNumber(smsRequest.getReceiverPhoneNumber()),
+                                new PhoneNumber(twilioConfiguration.getTrial_number()), "Account balance request. Ksh" + accountNumber.getAccountBalance())
+                                .create();
 
                         return UniversalResponse.builder().message("Balance request successful account balance:" + accountNumber.getAccountBalance()).build();
                     }
@@ -91,6 +72,9 @@ public class CheckBalanceService {
                 catch (Exception ex){
                     return UniversalResponse.builder().message("Transaction Error").status(0).build();
                 }
+            }
+            else {
+                return UniversalResponse.builder().message("Wrong Pin!").status(0).build();
             }
         }
         catch (Exception e) {
