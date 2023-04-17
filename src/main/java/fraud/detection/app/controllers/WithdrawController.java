fd6ee009a0d4f39014ff96831edf077087d39719
kevi-t <kevinwafula716@gmail.com>
2023-04-17 11:56:54 +0300

    update withdraw service

--- src/main/java/fraud/detection/app/controllers/WithdrawController.java
+++ src/main/java/fraud/detection/app/controllers/WithdrawController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/fraud/app")
+@RequestMapping("/fraud/app/account")
 @Validated
 @Slf4j
 public class WithdrawController {
--- src/main/java/fraud/detection/app/services/WithdrawService.java
+++ src/main/java/fraud/detection/app/services/WithdrawService.java
@@ -7,17 +7,13 @@ import fraud.detection.app.dto.SendMoneyMessage;
 import fraud.detection.app.dto.WithdrawDTO;
 import fraud.detection.app.models.Account;
 import fraud.detection.app.models.Transaction;
-import fraud.detection.app.models.User;
 import fraud.detection.app.repositories.AccountRepository;
 import fraud.detection.app.repositories.TransactionRepository;
-import fraud.detection.app.repositories.UserRepository;
 import fraud.detection.app.responses.UniversalResponse;
+import fraud.detection.app.utils.HelperUtility;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @Slf4j
@@ -26,50 +22,35 @@ public class WithdrawService {
     private final TwilioConfiguration twilioConfig;
     private  final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     public  UniversalResponse response;
+    public final HelperUtility helperUtility;
+    String referenceCode = HelperUtility.referenceCodeGenerator();
 
     @Autowired
-    public WithdrawService(TwilioConfiguration twilioConfig, AccountRepository accountRepository, TransactionRepository transactionRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public WithdrawService(TwilioConfiguration twilioConfig,
+                           AccountRepository accountRepository,
+                           TransactionRepository transactionRepository,
+                           HelperUtility helperUtility) {
         this.twilioConfig = twilioConfig;
         this.accountRepository = accountRepository;
         this.transactionRepository = transactionRepository;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        this.helperUtility = helperUtility;
     }
     public UniversalResponse withdrawMoney(WithdrawDTO request) {
 
         try{
             Account accountNumber2 = accountRepository.findByAccountNumber(request.getAccountNumber());
-            String EnteredPin = request.getPin();
-            User user = userRepository.findUserBymobileNumber(request.getAccountNumber());
-            String DatabasePin = user.getPin();
 
-            if (accountNumber2 == null) {
-                return UniversalResponse.builder().message("Account not found, please try again").build();
-            }
-            else if (passwordEncoder.matches(EnteredPin, DatabasePin)) {
-                System.out.println("You Entered The wrong Pin");
-                return UniversalResponse.builder().message("You entered the wrong pin!").status(0).build();
-            }
-            else{
+            if (helperUtility.checkPin(request.getPin(),request.getAccountNumber())) {
+
                 double inputAmount = request.getTransactionAmount();
                 double currentBalance = accountNumber2.getAccountBalance();
-                //generating unique reference code
-                UUID uuid = UUID.randomUUID();
-                String randomUUIDString = uuid.toString();
-                String referenceCode = "TUCN" + randomUUIDString;
 
                 if (inputAmount >= currentBalance){
                     var trans = Transaction.builder()
                             .transactionAmount((float) request.getTransactionAmount())
                             .transactionType("WITHDRAW MONEY")
                             .ReferenceCode(referenceCode)
-                            .senderAccount(request.getAccountNumber())
-                            .receiverAccount(request.getAccountNumber())
-                            .Debited(request.getAccountNumber())
-                            .Credited(request.getAccountNumber())
                             .Status("1")
                             .build();
                     transactionRepository.save(trans);
@@ -88,8 +69,8 @@ public class WithdrawService {
                                 .transactionAmount(request.getTransactionAmount())
                                 .ReferenceCode(referenceCode)
                                 .transactionType("WITHDRAW")
-                                .Debited(request.getAccountNumber())
-                                .Credited(request.getAccountNumber())
+                                .Debited("-" + request.getTransactionAmount())
+                                .Credited("+" + request.getTransactionAmount())
                                 .Status("0")
                                 .build();
                         transactionRepository.save(transaction);
@@ -102,12 +83,14 @@ public class WithdrawService {
                                 .build();
                         System.out.println(smsRequest);
                         try {
-                            Message twilioMessage = Message.creator(new PhoneNumber(smsRequest.getReceiverPhoneNumber()),
-                                    new PhoneNumber(twilioConfig.getTrial_number()), "You have received. Ksh" + request.getTransactionAmount() + " From" + request.getAccountNumber()).create();
-                            twilioMessage = Message.creator(new PhoneNumber(smsRequest.getReceiverPhoneNumber()),
-                                    new PhoneNumber(twilioConfig.getTrial_number()), "You have Sent. Ksh" + request.getTransactionAmount() + " To" + request.getAccountNumber()).create();
+                            Message.creator(new PhoneNumber(smsRequest.getReceiverPhoneNumber()),
+                                            new PhoneNumber(twilioConfig.getTrial_number()),
+                                            "You have withdrawn. Ksh" + request.getTransactionAmount() +" From" + request.getAccountNumber())
+                                    .create();
 
-                            return  UniversalResponse.builder().message("Withdraw Request of Amount:"+request.getTransactionAmount()+"Successful new account Balance: "+accountNumber2.getAccountBalance()).build();
+                            return  UniversalResponse.builder().message("Withdraw Request of Amount:"+request.getTransactionAmount()+"Successful new account Balance: "
+                                    +accountNumber2.getAccountBalance())
+                                    .build();
                         }
                         catch (Exception ex) {
                             System.out.println("Error While Sending Transaction Message" + ex);
@@ -116,20 +99,22 @@ public class WithdrawService {
                     }
                     catch (Exception ex){
                         ex.printStackTrace();
+
                         var trans = Transaction.builder()
                                 .transactionAmount((float) request.getTransactionAmount())
                                 .transactionType("WITHDRAW MONEY")
                                 .ReferenceCode(referenceCode)
                                 .senderAccount(request.getAccountNumber())
                                 .receiverAccount(request.getAccountNumber())
-                                .Debited(request.getAccountNumber())
-                                .Credited(request.getAccountNumber())
                                 .Status("1")
                                 .build();
                         transactionRepository.save(trans);
                         return UniversalResponse.builder().message("Transaction Failed").status(0).build();
                     }
                 }
+            }
+            else{
+                return UniversalResponse.builder().message("Wrong pin!").status(0).build();
             }
         }
         catch (Exception e){
