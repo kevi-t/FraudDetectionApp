a40101a033f37bd2ddf0b68f3fd8d36241639f9f
Phillip <phillipmugambi68@gmail.com>
2023-04-25 10:34:17 +0300

    added

--- src/main/java/fraud/detection/app/services/AuthenticateService.java
+++ src/main/java/fraud/detection/app/services/AuthenticateService.java
@@ -2,10 +2,11 @@ package fraud.detection.app.services;
 
 
 import fraud.detection.app.dto.AuthenticationDTO;
+import fraud.detection.app.dto.LoginResponse;
+import fraud.detection.app.models.User;
 import fraud.detection.app.repositories.UserRepository;
 import fraud.detection.app.responses.UniversalResponse;
 import fraud.detection.app.utils.JwtTokenUtil;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,55 +18,70 @@ import static fraud.detection.app.utils.HelperUtility.checkPhoneNumber;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class AuthenticateService {
 
     private final JwtTokenUtil jwtTokenUtil;
     private final AuthenticationManager authenticationManager;
     private final UserRepository userRepository;
-    public UniversalResponse response;
+    private final UniversalResponse universalResponseresponse;
+    private final LoginResponse loginResponse;
+
+    public AuthenticateService(JwtTokenUtil jwtTokenUtil, AuthenticationManager authenticationManager, UserRepository userRepository, UniversalResponse universalResponseresponse, LoginResponse loginResponse) {
+        this.jwtTokenUtil = jwtTokenUtil;
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.universalResponseresponse = universalResponseresponse;
+        this.loginResponse = loginResponse;
+    }
 
     public UniversalResponse login(AuthenticationDTO request) {
-
+        String checkedNumber = checkPhoneNumber(request.getMobileNumber());
         try {
-            String checkedNumber = checkPhoneNumber(request.getMobileNumber());
-            if (userRepository.findUserByMobileNumber(checkedNumber)==null){
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(checkedNumber, request.getPin()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        catch (Exception ex){
+            log.info("Username or pin incorrect");
+            return  UniversalResponse.builder()
+                    .message("Username or pin incorrect")
+                    .status("1")
+                    .build();
+        }
+        try {
+            User user = userRepository.findUserByMobileNumber(checkedNumber);
+            if (user == null) {
+                log.info("User not found please register");
                 return  UniversalResponse.builder()
                         .message("User not found please register")
                         .status("1")
                         .build();
             }
             else {
-                try {
-                    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(checkedNumber, request.getPin()));
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                    try{
-                        String jwt = jwtTokenUtil.createToken(checkedNumber);
-                        return UniversalResponse.builder()
-                            .message("Login successful")
-                            .status("0")
-                            .data(jwt)
-                            .build();
-                    }
-                    catch (Exception ex){
+                try{
+                    String jwt = jwtTokenUtil.createToken(checkedNumber);
+                    loginResponse.setToken(jwt);
+                    loginResponse.setUserPhoneNumber(request.getMobileNumber());
+                    loginResponse.setUserEmail(user.getEmail().toString());
                     return UniversalResponse.builder()
-                            .message("Failed to generate token")
+                            .message("login sucessful")
                             .status("1")
+                            .data(loginResponse)
                             .build();
-                    }
                 }
                 catch (Exception ex){
-                    System.out.println(ex);
-                    return  UniversalResponse.builder()
-                            .message("Username or pin incorrect")
+                    log.info("Failed to generate token"+ex);
+                    return UniversalResponse.builder()
+                            .message("Failed to generate token"+ex)
                             .status("1")
                             .build();
                 }
             }
         }
         catch (Exception ex){
-            System.out.println(ex);
+            log.info("Error while finding user"+ex);
+            ex.printStackTrace();
         }
-        return response;
+        return universalResponseresponse;
     }
+
 }
